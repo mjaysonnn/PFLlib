a7bce907e8ec0a239d7cd311ddae7d8bfdd5af93
mjs <mjs8612@gmail.com>
2025-02-10 23:38:09 +0000

    update prepare.sh for silent Miniconda installation and Conda initialization

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
+
 now_path=`pwd`
 
-# Install miniconda
+# Install Miniconda (Silent Installation)
 cd
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sh Miniconda3-latest-Linux-x86_64.sh -b
-~/miniconda3/bin/conda init bash  # Add this line
+wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda.sh
+bash Miniconda.sh -b -p $HOME/miniconda
+
+# Add Conda to PATH (Temporary for this session)
+export PATH="$HOME/miniconda/bin:$PATH"
+
+# Initialize Conda (Required before using `conda activate`)
+conda init
+source ~/.bashrc  # Reload shell to apply Conda settings
 
 # Configure ~/.bashrc
 echo alias p=\"ps -aux|grep zhangjq|grep 'python -u'\" >> ~/.bashrc
@@ -15,9 +23,10 @@ echo alias del_pycache=\'find . -type d -name __pycache__ -prune -exec rm -rf {}
 echo export PIP_CACHE_DIR='$PWD'/tmp >> ~/.bashrc
 echo export TMPDIR='$PWD'/tmp >> ~/.bashrc
 
-# Install python packages
+# Install Python packages
 cd $now_path
-source ~/.bashrc
+source ~/.bashrc  # Ensure changes take effect
 conda env create -f env_cuda_latest.yaml
 
+# Activate Conda environment
 conda activate pfllib
