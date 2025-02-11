71590a3002de09d4199f5be2a5b5cfaaca5af09b
mjs <mjs8612@gmail.com>
2025-02-11 21:50:33 +0000

    add timestamp to result filenames in get_all_results_for_one_algo function

--- system/utils/result_utils.py
+++ system/utils/result_utils.py
@@ -18,6 +18,7 @@
 import h5py
 import numpy as np
 import os
+import time
 
 
 def average_data(algorithm="", dataset="", goal="", times=10):
@@ -33,9 +34,9 @@ def average_data(algorithm="", dataset="", goal="", times=10):
 
 def get_all_results_for_one_algo(algorithm="", dataset="", goal="", times=10):
     test_acc = []
-    algorithms_list = [algorithm] * times
+    timestamp = time.strftime("%Y%m%d-%H%M%S")  # Format: YYYYMMDD-HHMMSS
     for i in range(times):
-        file_name = dataset + "_" + algorithms_list[i] + "_" + goal + "_" + str(i)
+        file_name = f"{dataset}_{algorithm}_{goal}_{i}_{timestamp}"  # Add timestamp to filename
         test_acc.append(np.array(read_data_then_delete(file_name, delete=False)))
 
     return test_acc
