c61dd7de57f3df9e402cee2d2e72260f02784aff
mjs <mjs8612@gmail.com>
2025-05-20 13:13:15 +0000

    Automated commit: 2025-05-20 13:13:15

--- system/cifar100_mobilenet_subsampling_m20_non_iid_dir.sh
+++ system/cifar100_mobilenet_subsampling_m20_non_iid_dir.sh
@@ -2,26 +2,27 @@
 
 # Function to generate non-IID Dirichlet data for CIFAR-100
 generate_noniid_data() {
-    echo "Generating non-IID Dirichlet CIFAR-100 data with $1 clients..."
+    echo "Generating non-IID Dirichlet CIFAR-10 data with $1 clients..."
     cd ../dataset || exit
-    rm -rf Cifar100/
-    python generate_Cifar100.py noniid - dir $1
+    rm -rf Cifar10/
+    python generate_Cifar10.py noniid - dir $1
     cd ../system || exit
 }
 
 # Local Epochs = 10, Batch Size = 32, Learning Rate = 0.01
 
-# # Pure On-Demand (20 OD)
-# generate_noniid_data 100
-# python main.py -data Cifar100 -ncl 100 -m MobileNet -algo FedAvg -gr 250 -ls 10 -nc 100 -jr 0.2 \
-# --on_demand_clients 20 --spot_clients 0 -lr 0.01 -lbs 32 \
-# -go "MobileNet_cifar100_e10_dirichlet_p20q0"
+# Pure On-Demand (20 OD)
+generate_noniid_data 100
+python main.py -data Cifar10 -ncl 10 -m MobileNet -algo FedAvg -gr 250 -ls 10 -nc 100 -jr 0.2 \
+--on_demand_clients 20 --spot_clients 0 -lr 0.01 -lbs 32 \
+-go "MobileNet_cifar10_e10_dirichlet_p20q0"
 
-# # Balanced (10 OD + 20 Spot)
-# generate_noniid_data 100
-# python main.py -data Cifar100 -ncl 100 -m MobileNet -algo FedAvg -gr 250 -ls 10 -nc 100 -jr 0.3 \
-# --on_demand_clients 10 --spot_clients 20 -lr 0.01 -lbs 32 \
-# -go "MobileNet_cifar100_e10_dirichlet_p10q20"
+exit
+# Balanced (10 OD + 20 Spot)
+generate_noniid_data 100
+python main.py -data Cifar100 -ncl 100 -m MobileNet -algo FedAvg -gr 250 -ls 10 -nc 100 -jr 0.3 \
+--on_demand_clients 10 --spot_clients 20 -lr 0.01 -lbs 32 \
+-go "MobileNet_cifar100_e10_dirichlet_p10q20"
 
 # # Intermediate 1 (8 OD + 22 Spot)
 # generate_noniid_data 100
